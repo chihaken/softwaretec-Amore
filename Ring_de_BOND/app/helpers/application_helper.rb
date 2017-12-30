3e60cab2dd61e8b7f0ba4b8c150ff9a5b4b8e44f
chihara <190338p@ugs.kochi-tech.ac.jp>
2017-12-30 23:37:57 +0900

    add menubar version

--- Ring_de_BOND/app/helpers/application_helper.rb
+++ Ring_de_BOND/app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "Ring De BOND"
+    if page_title. empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def menu_link_to(text, path)
+    link_to_unless_current(text, path){ content_tag(:span,text)}
+  end
+
 end
